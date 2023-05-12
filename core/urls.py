86fd3b419f0f313a7b8c8b2a6c20f2bc19c0d75a
taysa-fernandes <taysamirelly34@gmail.com>
2023-05-12 10:23:44 -0300

    aproveitando url

--- core/urls.py
+++ core/urls.py
@@ -2,15 +2,11 @@ from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 
 
-from .views import PessoaListView, PessoaCreateView, PessoaUpdateView, PessoaDestroyView,PessoaViewSet
+from .views import PessoaViewSet
 
 router = DefaultRouter()
-router.register(r'pessoas', PessoaViewSet, basename='pessoa')
+router.register(r'pessoa', PessoaViewSet, basename='pessoa')
 
 urlpatterns = [
-    path('pessoas/',PessoaListView.as_view(),name='pessoa'),
-    path('pessoas/create/', PessoaCreateView.as_view(), name='pessoa-create'),
-    path('pessoas/<int:pk>/update/', PessoaUpdateView.as_view(),name='pessoa-update'),
-    path('pessoas/<int:pk>/delete/', PessoaDestroyView.as_view(),name='pessoa-delete'),
-
-]
+   
+] + router.urls
--- core/views.py
+++ core/views.py
@@ -9,26 +9,3 @@ from .serializers import PessoaSerializer
 class PessoaViewSet(viewsets.ModelViewSet):
     queryset = Pessoa.objects.all()
     serializer_class = PessoaSerializer
-
-class PessoaListView(generics.ListCreateAPIView):
-    queryset = Pessoa.objects.all()
-    serializer_class = PessoaSerializer
-
-class PessoaDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Pessoa.objects.all()
-    serializer_class = PessoaSerializer
-
-class PessoaCreateView(generics.CreateAPIView):
-    queryset = Pessoa.objects.all()
-    serializer_class = PessoaSerializer
-    permission_classes = [AllowAny] 
-
-class PessoaUpdateView(generics.UpdateAPIView):
-    queryset = Pessoa.objects.all()
-    serializer_class = PessoaSerializer
-    permission_classes = [AllowAny] 
-
-class PessoaDestroyView(generics.DestroyAPIView):
-    queryset = Pessoa.objects.all()
-    serializer_class = PessoaSerializer
-    permission_classes = [AllowAny] 
